623fbd22944871ad63a8c528776d4ebf2bd2911e
Praffulla Dubey <praffullakrdubey@gmail.com>
2021-01-27 13:52:06 +0530

    added add

---
+++ 2D Arrays/Add.java
@@ -0,0 +1,49 @@
+/*Matrix Addition*/
+import java.io.*;
+import java.util.*;
+public class I30{
+    public static void main(String args[])throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[][] = new int[n][n];
+        int brr[][] = new int[n][n];
+        int crr[][] = new int[n][n];
+        int temp;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("ARR");
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                brr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("BRR");
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("Addition");
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                crr[i][j] = arr[i][j] + brr[i][j];
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(crr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
