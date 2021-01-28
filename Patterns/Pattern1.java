a8f6cbf0c7a2d8734e6f0a5b2fffee28a66b0cc7
Praffulla Dubey <praffullakrdubey@gmail.com>
2021-01-28 10:18:19 +0530

    Added Patter 1

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,19 @@
+/*Pyramind Pattern*/
+import java.io.*;
+import java.util.*;
+public class I33{
+    public static void main(String args[])throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            for(int j = n-i; j > 1; j--){
+                System.out.print(" ");
+            }
+            for(int j = 0; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+        
